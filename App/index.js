8fc34ee16a98673754ec9cd638d564fd98186f37
Bardia Jedi <Bardia.Jedi@enfogroup.com>
2021-09-11 17:09:23 +0200

    app server connection

--- App/index.js
+++ App/index.js
@@ -1,11 +1,32 @@
+const got = require('got')
 const express = require('express')
+// import express from 'express'
 app = express()
-const port = 3000
+
+
+
+const port = process.env['port'] || 8080
+const server = process.env['server'] || "http://localhost"
+const server_port = process.env['server_port'] || 12345
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
+})
+
+app.get('/test', (req, res) => {
+    res.send({ test: "hello world" });
+})
+
+app.get("/srv", (req, res, next) => {
+    try {
+        console.log(server_port)
+        got.get(`${server}:${server_port}/test`).json().then(j => res.send(j));
+    }catch(e){
+        res.send(e)
+    }
+
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
